88f1de1aa2c98239e0c728fcddbe0a33210538b4
Nguyen Manh Tung <tung.nguyen@scopicsoftware.com>
2018-10-06 10:34:41 +0700

    LSP principle

---
+++ SOLID/SOLID.LiskovSubstitutionPrinciple.RightDesign/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace SOLID.LiskovSubstitutionPrinciple.RightDesign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // staff là một kiểu của employee
+            // do vậy có thể được sử dụng như employee
+            IEmployee staff = new Staff();
+
+            // manager cũng là một kiểu của employee
+            IEmployee manager = new Manager();
+
+            Console.WriteLine("Salary of staff = " + staff.CalculateSalary());
+            Console.WriteLine("Salary of manager = " + manager.CalculateSalary());
+            Console.ReadLine();
+        }
+    }
+
+    public interface IEmployee
+    {
+        double CalculateSalary();
+    }
+
+    public class Staff : IEmployee
+    {
+        public double CalculateSalary()
+        {
+            return 10.0;
+        }
+    }
+
+    public class Manager : IEmployee
+    {
+        public double CalculateSalary()
+        {
+            return 20.0;
+        }
+    }
+}
---
+++ SOLID/SOLID.LiskovSubstitutionPrinciple.WrongDesign/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SOLID.LiskovSubstitutionPrinciple.WrongDesign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Square();
+            rectangle.Height = 5;
+            rectangle.Width = 10;
+
+            // square không thể thay thế rectangle
+            // do vậy đây là một thiết kế sai
+            Console.WriteLine("Square = "+ rectangle.CalculateSquare());
+            Console.ReadLine();
+        }
+    }
+
+    public class Rectangle
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public int CalculateSquare()
+        {
+            return Width * Height;
+        }
+    }
+
+    public class Square : Rectangle
+    {
+        public int Side { get; set; }
+    }
+}
