06eb5bf78b30562d8f9d0d822d02d87563bca585
Nguyen Manh Tung <tung.nguyen@scopicsoftware.com>
2018-10-08 10:30:06 +0700

    SRP Imp

---
+++ SOLID/SOLID.SingleResponsibility/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace SOLID.SingleResponsibility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+            customer.Add();
+            Console.ReadLine();
+        }
+    }
+
+
+    class FileLogger
+    {
+        public void Handle(string error)
+        {
+            System.IO.File.WriteAllText(@"c:\Error.txt", error);
+        }
+    }
+
+    class Customer
+    {
+        private FileLogger logger = new FileLogger();
+        public virtual void Add()
+        {
+            try
+            {
+                Console.WriteLine("Customer is addeed to database");
+            }
+            catch (Exception ex)
+            {
+                // Lớp customer chỉ việc thêm customer vào database
+                // Khi exception xảy ra, FileLogger sẽ có nhiệm vụ ghi
+                // lại exception, lớp Customer không cần quan tâm về điều đó
+                // Khi muốn thay đổi phương thức ghi log, như chuyển từ file txt
+                // vào file Json hay database, bạn chỉ việc thay đổi lớp Logger
+                logger.Handle(ex.ToString());
+            }
+        }
+    }
+}
